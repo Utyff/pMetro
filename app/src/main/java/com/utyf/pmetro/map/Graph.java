5737a03b22cdf544184ade675f6724910d73818b
Fedor Ermishin <ermishinf2@gmail.com>
2016-03-16 22:56:48 +0300

    Performance optimizations of path finding

--- app/src/main/java/com/utyf/pmetro/map/Graph.java
+++ app/src/main/java/com/utyf/pmetro/map/Graph.java
@@ -8,15 +8,22 @@ import java.util.PriorityQueue;
 
 /**
  * Created by Fedor on 13.03.2016.
+ *
+ * Weighted directed graph and shortest paths finding within it
  */
 
 public class Graph<Node> {
     private HashMap<Node, Integer> vertexIndices;
     private ArrayList<Node> vertices;
     private ArrayList<ArrayList<EdgeInfo>> edges;
-    private double[] distances;
-    private int[] parents;
     private int nVertices;
+
+    // temporal data
+    private int[] parents;
+    private PriorityQueue<Integer> queue;
+
+    // output data
+    private double[] distances;
 
     class EdgeInfo {
         public int toIdx;
@@ -54,44 +61,57 @@ public class Graph<Node> {
         edges.get(fromIdx).add(new EdgeInfo(toIdx, weight));
     }
 
+    // Compute shortest paths using Dijkstra's algorithm
     public void computeShortestPaths(Node[] startVertices) {
-        distances = new double[nVertices];
-        Arrays.fill(distances, Double.POSITIVE_INFINITY);
-        parents = new int[nVertices];
+        // Reuse allocated memory
+        if (distances == null || distances.length < nVertices) {
+            distances = new double[nVertices];
+            parents = new int[nVertices];
+            queue = new PriorityQueue<>(nVertices,
+                    new Comparator<Integer>() {
+                        @Override
+                        public int compare(Integer lhs, Integer rhs) {
+                            return Double.compare(distances[lhs], distances[rhs]);
+                        }
+                    });
+        }
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>(nVertices,
-                new Comparator<Integer>() {
-                    @Override
-                    public int compare(Integer lhs, Integer rhs) {
-                        return Double.compare(distances[lhs], distances[rhs]);
-                    }
-                });
+        Arrays.fill(distances, Double.POSITIVE_INFINITY);
         for (Node node: startVertices) {
             int idx = getVertexIndex(node);
             distances[idx] = 0;
             queue.offer(idx);
         }
+        // Contains value for each vertex, indicating if it has been already extracted from the queue
+        boolean isVisited[] = new boolean[nVertices];
+
         while (!queue.isEmpty()) {
             int fromIdx = queue.poll();
+            // This check increases queue size, but allows not to use "decrease key" operation
+            if (isVisited[fromIdx])
+                continue;
+            isVisited[fromIdx] = true;
             for (EdgeInfo edgeInfo: edges.get(fromIdx)) {
                 int toIdx = edgeInfo.toIdx;
                 double newDistance = distances[fromIdx] + edgeInfo.weight;
                 if (distances[toIdx] > newDistance) {
                     distances[toIdx] = newDistance;
                     parents[toIdx] = fromIdx;
-                    // remove has linear complexity, so it can slow down path finding
-                    queue.remove(toIdx);
                     queue.offer(toIdx);
                 }
             }
         }
     }
 
+    // Compute length of path from start vertex to end vertex. Shortest paths must be already
+    // precomputed by calling computeShortestPaths.
     public double getPathLength(Node endVertex) {
         int idx = getVertexIndex(endVertex);
         return distances[idx];
     }
 
+    // Get next to last vertex on the shortest path from start vertex to v. Shortest paths must be
+    // already precomputed by calling computeShortestPaths.
     public Node getParent(Node v) {
         int idx = getVertexIndex(v);
         return vertices.get(parents[idx]);
